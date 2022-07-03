864ac1a1c15572a01085370810330b21f559400c
Jiseon Jang <jsmeaningood@gmail.com>
2022-07-03 15:27:58 +0900

    22.07.03

--- Day_5_0628-0706/G3_1613.py
+++ Day_5_0628-0706/G3_1613.py
@@ -1,6 +1,14 @@
 import sys
 input =  lambda : sys.stdin.readline().strip()
 
+'''
+1. n * n의 2차원 배열 생성
+2. for문 다 돌면서 각 i의 dfs 돌기
+3. 시작 s, 끝 e로 뒀을 때 visited[s][e]가 True면 s -> e로 이동 했다는 뜻
+4. visited[e][s]가 True면 e -> s로 이동했다는 뜻
+5. 둘 다 아니라면 관련이 없는 경우이므로 0
+
+'''
 
 def dfs(idx, cur):
     visited[idx][cur] = True
@@ -14,11 +22,9 @@ def dfs(idx, cur):
 n, m = map(int, input().split())
 
 v = [[] for _ in range(n + 1)]
-rv = [[] for _ in range(n + 1)]
 for i in range(m):
     a, b = map(int, input().split())
     v[a].append(b)
-    rv[b].append(a)
     
     
 visited= [[False for i in range(n + 1)] for j in range(n + 1)]
--- Day_5_0628-0706/S1_11497.py
+++ Day_5_0628-0706/S1_11497.py
@@ -1,6 +1,12 @@
 import sys
 input = lambda : sys.stdin.readline().strip()
 
+'''
+1. 가장 낮은 길이의 통나무와 가장 높은 길이의 통나무는 인접하지 말아야 함
+2. 즉, 어중간한 길이들의 통나무들 사이에 끼우기
+3. 양 옆으로 두 개씩 통나무가 둘러져 있으므로
+4. 정렬 후 [idx - 2]의 길이가 최대값인 경우를 출력하면 끝
+'''
 
 T = int(input())
 for tc in range(T):
