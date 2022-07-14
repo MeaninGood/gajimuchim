3e16ac06c1edf11a96f2813754da24c35dd06c73
장지선 <wls4845@naver.com>
2022-07-14 23:06:26 +0900

    22.07.14

--- Day_7_0713-0715/S2_1283.py
+++ Day_7_0713-0715/S2_1283.py
@@ -5,25 +5,30 @@ input = lambda : sys.stdin.readline().strip()
 
 n = int(input())
 arr = [input() for _ in range(n)]
-print(arr)
 
+# 스플릿 해서 넣기
 for i in range(n):
     arr[i] = arr[i].split(' ')
 
+# 단축키 지정됐는지 체크할 배열
 visited = [False for i in range(n)]
+# 단축키 확인 배열
 d = {}
 for i in range(n):
     for j in range(len(arr[i])):
-        print(arr[i][j][0])
+        # 단축키가 있으면 넘김, 첫단어부터 확인
         if arr[i][j][0].upper() in d:
             continue
         
         else:
+            # 단축키가 없으면 딕셔너리에 대문자로 추가해주고
             d[arr[i][j][0].upper()] = d.get(arr[i][j][0].upper(), 1)
+            # 단축키 지정된 배열이라고 표시
             visited[i] = True
+            # 배열 바꿔주고 break
             arr[i][j] = '[' + arr[i][j][0] + ']' + arr[i][j][1:]
             break
-
+    # 위의 과정에서 단축키 지정되지 않았으면 단어마다 글자 하나씩 확인해야 함
     if not visited[i]:
         for j in range(len(arr[i])):
             for k in range(len(arr[i][j])):
@@ -35,18 +40,9 @@ for i in range(n):
                     visited[i] = True
                     arr[i][j] = arr[i][j][:k] + '[' + arr[i][j][k] + ']' + arr[i][j][k + 1:]
                     break
-
+            # 단축키 지정되었으면 끝내고, 지정되지 않았으면 다음 배열로 넘어감
             if visited[i]:
                 break
 
 for i in arr:
     print(*i)
-# c = ['a', 'bvv', 'c']
-# # c[1][1] = c[1][1].replace(c[1][1], a)
-# tmp = c[1][1]
-# print(tmp)
-# print(c[1][:1])
-# print(c[1][2:])
-# print
-# c[1] = c[1][:1] + '[' + tmp + ']' + c[1][2:]
-# print(c)
