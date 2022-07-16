06ecce22c4d67c06650d2f1093f198600f28ef5a
장지선 <wls4845@naver.com>
2022-07-16 20:10:28 +0900

    22.07.16

---
+++ Day_8_0716-0719/G4_1253.py
@@ -0,0 +1,37 @@
+# 하오
+
+import sys
+input = lambda : sys.stdin.readline().strip()
+
+n = int(input())
+arr = sorted(list(map(int, input().split())))
+
+idx = n - 1
+ans = 0
+while idx > -1:
+    target = arr[idx]
+    s = 0
+    e = n - 1
+    while s < e:
+        if arr[s] + arr[e] == target:   # 같을 때 e나 s가 idx와 같으면 pass
+            if e == idx:
+                e -= 1
+                continue
+            
+            elif s == idx:
+                s += 1
+                continue
+
+            else:
+                ans += 1
+                break
+
+        elif arr[s] + arr[e] > target:
+            e -= 1
+        
+        else:
+            s += 1
+
+    idx -= 1
+
+print(ans)
